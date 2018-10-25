7dd537ce0ce3405a240eb0b06b3abc376bcb8233
Ivan Contreras <ivancontry.13@gmail.com>
2018-10-25 14:42:51 -0500

    menu Usuarios

--- Presentacion/Principal.Designer.cs
+++ Presentacion/Principal.Designer.cs
@@ -30,14 +30,33 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
             this.menuHorizontal = new System.Windows.Forms.Panel();
-            this.menuVertical = new System.Windows.Forms.Panel();
-            this.menuContenedor = new System.Windows.Forms.Panel();
             this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.menuVertical = new System.Windows.Forms.Panel();
+            this.panelBtnOpciones = new System.Windows.Forms.Panel();
+            this.panelDispositivo = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.btnOpciones = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGestionDispositivo = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuContenedor = new System.Windows.Forms.Panel();
+            this.panelOpciones = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
             this.menuHorizontal.SuspendLayout();
-            this.menuVertical.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.menuVertical.SuspendLayout();
+            this.panelDispositivo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuContenedor.SuspendLayout();
+            this.panelOpciones.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuHorizontal
@@ -50,25 +69,6 @@
             this.menuHorizontal.Size = new System.Drawing.Size(800, 38);
             this.menuHorizontal.TabIndex = 0;
             // 
-            // menuVertical
-            // 
-            this.menuVertical.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.menuVertical.Controls.Add(this.pictureBox1);
-            this.menuVertical.Dock = System.Windows.Forms.DockStyle.Left;
-            this.menuVertical.Location = new System.Drawing.Point(0, 38);
-            this.menuVertical.Name = "menuVertical";
-            this.menuVertical.Size = new System.Drawing.Size(220, 412);
-            this.menuVertical.TabIndex = 1;
-            // 
-            // menuContenedor
-            // 
-            this.menuContenedor.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.menuContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.menuContenedor.Location = new System.Drawing.Point(220, 38);
-            this.menuContenedor.Name = "menuContenedor";
-            this.menuContenedor.Size = new System.Drawing.Size(580, 412);
-            this.menuContenedor.TabIndex = 2;
-            // 
             // btnCerrar
             // 
             this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
@@ -79,6 +79,93 @@
             this.btnCerrar.TabIndex = 0;
             this.btnCerrar.TabStop = false;
             // 
+            // menuVertical
+            // 
+            this.menuVertical.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuVertical.Controls.Add(this.panelBtnOpciones);
+            this.menuVertical.Controls.Add(this.panelDispositivo);
+            this.menuVertical.Controls.Add(this.btnOpciones);
+            this.menuVertical.Controls.Add(this.panel1);
+            this.menuVertical.Controls.Add(this.btnGestionDispositivo);
+            this.menuVertical.Controls.Add(this.pictureBox1);
+            this.menuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuVertical.Location = new System.Drawing.Point(0, 38);
+            this.menuVertical.Name = "menuVertical";
+            this.menuVertical.Size = new System.Drawing.Size(220, 412);
+            this.menuVertical.TabIndex = 1;
+            this.menuVertical.Paint += new System.Windows.Forms.PaintEventHandler(this.menuVertical_Paint);
+            // 
+            // panelBtnOpciones
+            // 
+            this.panelBtnOpciones.BackColor = System.Drawing.Color.Lime;
+            this.panelBtnOpciones.Location = new System.Drawing.Point(12, 202);
+            this.panelBtnOpciones.Name = "panelBtnOpciones";
+            this.panelBtnOpciones.Size = new System.Drawing.Size(5, 30);
+            this.panelBtnOpciones.TabIndex = 4;
+            // 
+            // panelDispositivo
+            // 
+            this.panelDispositivo.Controls.Add(this.panel12);
+            this.panelDispositivo.Controls.Add(this.button10);
+            this.panelDispositivo.Location = new System.Drawing.Point(12, 164);
+            this.panelDispositivo.Name = "panelDispositivo";
+            this.panelDispositivo.Size = new System.Drawing.Size(213, 38);
+            this.panelDispositivo.TabIndex = 1;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.Lime;
+            this.panel12.Location = new System.Drawing.Point(14, 3);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(5, 30);
+            this.panel12.TabIndex = 4;
+            // 
+            // button10
+            // 
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(14, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(194, 30);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // btnOpciones
+            // 
+            this.btnOpciones.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnOpciones.FlatAppearance.BorderSize = 0;
+            this.btnOpciones.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.btnOpciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpciones.Location = new System.Drawing.Point(15, 202);
+            this.btnOpciones.Name = "btnOpciones";
+            this.btnOpciones.Size = new System.Drawing.Size(205, 30);
+            this.btnOpciones.TabIndex = 3;
+            this.btnOpciones.Text = "button2";
+            this.btnOpciones.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lime;
+            this.panel1.Location = new System.Drawing.Point(12, 128);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 30);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnGestionDispositivo
+            // 
+            this.btnGestionDispositivo.FlatAppearance.BorderSize = 0;
+            this.btnGestionDispositivo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.btnGestionDispositivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestionDispositivo.Location = new System.Drawing.Point(12, 128);
+            this.btnGestionDispositivo.Name = "btnGestionDispositivo";
+            this.btnGestionDispositivo.Size = new System.Drawing.Size(208, 30);
+            this.btnGestionDispositivo.TabIndex = 1;
+            this.btnGestionDispositivo.Text = "button1";
+            this.btnGestionDispositivo.UseVisualStyleBackColor = true;
+            this.btnGestionDispositivo.Click += new System.EventHandler(this.btnGestionDispositivo_Click);
+            // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
@@ -87,6 +174,111 @@
             this.pictureBox1.Size = new System.Drawing.Size(220, 103);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // menuContenedor
+            // 
+            this.menuContenedor.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.menuContenedor.Controls.Add(this.panelOpciones);
+            this.menuContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuContenedor.Location = new System.Drawing.Point(220, 38);
+            this.menuContenedor.Name = "menuContenedor";
+            this.menuContenedor.Size = new System.Drawing.Size(580, 412);
+            this.menuContenedor.TabIndex = 2;
+            // 
+            // panelOpciones
+            // 
+            this.panelOpciones.Controls.Add(this.panel7);
+            this.panelOpciones.Controls.Add(this.button6);
+            this.panelOpciones.Controls.Add(this.panel6);
+            this.panelOpciones.Controls.Add(this.button5);
+            this.panelOpciones.Controls.Add(this.panel5);
+            this.panelOpciones.Controls.Add(this.button4);
+            this.panelOpciones.Controls.Add(this.panel4);
+            this.panelOpciones.Controls.Add(this.button3);
+            this.panelOpciones.Location = new System.Drawing.Point(137, 223);
+            this.panelOpciones.Name = "panelOpciones";
+            this.panelOpciones.Size = new System.Drawing.Size(213, 146);
+            this.panelOpciones.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Lime;
+            this.panel7.Location = new System.Drawing.Point(14, 111);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(5, 30);
+            this.panel7.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(14, 111);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 30);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Lime;
+            this.panel6.Location = new System.Drawing.Point(14, 75);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(5, 30);
+            this.panel6.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(14, 75);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(196, 30);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Lime;
+            this.panel5.Location = new System.Drawing.Point(14, 39);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(5, 30);
+            this.panel5.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(14, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(196, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Lime;
+            this.panel4.Location = new System.Drawing.Point(14, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(5, 30);
+            this.panel4.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Blue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(14, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Principal
             // 
@@ -100,9 +292,12 @@
             this.Text = "Principal";
             this.Load += new System.EventHandler(this.Principal_Load);
             this.menuHorizontal.ResumeLayout(false);
-            this.menuVertical.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.menuVertical.ResumeLayout(false);
+            this.panelDispositivo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuContenedor.ResumeLayout(false);
+            this.panelOpciones.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -114,5 +309,21 @@
         private System.Windows.Forms.Panel menuVertical;
         private System.Windows.Forms.Panel menuContenedor;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGestionDispositivo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelBtnOpciones;
+        private System.Windows.Forms.Button btnOpciones;
+        private System.Windows.Forms.Panel panelOpciones;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panelDispositivo;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Button button10;
     }
 }
--- Presentacion/Principal.cs
+++ Presentacion/Principal.cs
@@ -26,5 +26,36 @@ namespace Presentacion
         {
             
         }
+
+        private void menuVertical_Paint(object sender, PaintEventArgs e)
+        {
+           
+
+        }
+
+        private void btnGestionDispositivo_Click(object sender, EventArgs e)
+        {
+            if (panelDispositivo.Visible == false)
+            {
+                panelDispositivo.Visible = true;
+                btnOpciones.Location = new Point(15,163);
+            }
+            else {
+                panelDispositivo.Visible = false;
+                posicionarMenuLocal();
+            }
+        }
+
+        public void posicionarMenuLocal() {
+          
+            if (panelDispositivo.Visible)
+            {
+                //panelDispositivo.Visible = false;
+                //panelOpciones.Visible = false;
+                btnOpciones.Location = new Point(15, 163);
+                panelBtnOpciones.Location = new Point(12, 163);
+            }
+            
+        }
     }
 }
